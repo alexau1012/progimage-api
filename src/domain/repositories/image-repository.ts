835795f5ae9e2a140fab17ef5593b814972ee8fc
alexau1012 <alexaumh@gmail.com>
2023-06-24 13:24:52 +1000

    Remove commented code

--- src/domain/repositories/image-repository.ts
+++ src/domain/repositories/image-repository.ts
@@ -20,33 +20,6 @@ export const imageRepository = (
     storeImage: async (buffer, id) => {
       return imageDataSource.storeImage(buffer, id);
     },
-    // getImage: async (id, fileType) => {
-    //   const orignalData = await imageDataSource.getImage(id);
-
-    //   if (!orignalData) {
-    //     throw Error("NOT_FOUND");
-    //   }
-
-    //   const formattedImageBuffer = await sharp(orignalData.buffer)
-    //     [fileType]()
-    //     .toBuffer();
-
-    //   return {
-    //     id,
-    //     fileType,
-    //     buffer: formattedImageBuffer,
-    //   };
-    // },
-    // processImage: async (image, width, height, angle) => {
-    //   const processedBuffer = await sharp(image.buffer)
-    //     .resize(width, height)
-    //     .rotate(angle)
-    //     .toBuffer();
-    //   return {
-    //     ...image,
-    //     buffer: processedBuffer,
-    //   };
-    // },
     getProcessedImage: async (id, fileType, width, height, angle) => {
       const orignalData = await imageDataSource.getImage(id);
 
